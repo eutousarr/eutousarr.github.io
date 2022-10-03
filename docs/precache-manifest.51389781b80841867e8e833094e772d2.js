edb5f26238ea0a108febd041a4311d8b4f839dba
Ibrahima Sarr <kisarrweb@gmail.com>
2022-10-03 13:02:04 +0000

    docs folder created

---
+++ docs/precache-manifest.51389781b80841867e8e833094e772d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b3c159a5e56e01340aa9f1af83c3e93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40c06cfae92a981cfa7d",
+    "url": "/static/css/main.7fe69a39.chunk.css"
+  },
+  {
+    "revision": "5d33a81ee66152faad16",
+    "url": "/static/js/2.57623c95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.57623c95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40c06cfae92a981cfa7d",
+    "url": "/static/js/main.6710da18.chunk.js"
+  },
+  {
+    "revision": "20db760282bf9b487582",
+    "url": "/static/js/runtime-main.d8f04c90.js"
+  },
+  {
+    "revision": "e7030892a8135278b3f3dafcf1d57e06",
+    "url": "/static/media/logo.e7030892.jpeg"
+  },
+  {
+    "revision": "fc080f5fffc5f2105e63ed5ad83bfd83",
+    "url": "/static/media/signed-out-home.fc080f5f.jpg"
+  }
+]);
